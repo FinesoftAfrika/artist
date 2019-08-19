24f87e6536d9de3d85fdd6674673570ab726386f
Ej <Ejgithinji@gmail.com>
2019-08-19 10:42:34 +0300

    added details to the footer

--- src/components/PageFooter.js
+++ src/components/PageFooter.js
@@ -4,11 +4,24 @@ import config from '../../config';
 export default function PageFooter() {
   return (
     <footer id="footer">
+      <ul>
+        <input />
+        <button type="button">JOIN US</button>
+      </ul>
+      <ul>
+        <button type="button">Youtube</button>
+        <button type="button">Follow</button>
+      </ul>
       <ul className="copyright">
-        <li>&copy; {config.authorName}</li>
+        <li>&copy; 2019 Dream Nation |</li>
+        <li> All Rights Reserved. |</li>
+        <li> Send us Feedback |</li>
+        <li> Privacy Policy |</li>
+        <li> Terms and Conditions |</li>
         <li>
+          Built by
           <a href="https://www.finesoftafrika.com">Finesoft Afrika</a>  {' '}
-          <a href="#">  2019</a>
+
         </li>
       </ul>
     </footer>
